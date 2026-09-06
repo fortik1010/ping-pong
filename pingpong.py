d2604338b5f97d553c75e0b46fdebe2e5fb84941
fortik1010 <unity.life.compyter@gmail.com>
2026-09-06 18:17:42 +0300

    Added player class

--- pingpong.py
+++ pingpong.py
@@ -10,6 +10,16 @@ class GameSprite(sprite.Sprite):
         self.rect.y = player_y
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 0:
+            self.rect.x -= self.player_speed
+        if keys[K_RIGHT] and self.rect.x < 700 - 100:
+            self.rect.x += self.player_speed
+    def shoot(self):
+        bullet1 = Bullet('bullet.png', self.rect.centerx, self.rect.top, 15, 10, 10)
+        bullets.add(bullet1)
         
 game = True
 while game:
